80b1088f1851d997853d7bdec054e1337a854d6a
mahmud <mahmudskarim@gmail.com>
2019-11-05 14:28:24 -0500

    mergersort done

--- SortPractice/src/com/revature/sort/MergeSort.java
+++ SortPractice/src/com/revature/sort/MergeSort.java
@@ -1,12 +1,39 @@
 package com.revature.sort;
 
+import java.util.Arrays;
+
 public class MergeSort {
 	
 	public static void sort(int[] arr) {
 		// TODO Auto-generated method stub
+		if (arr.length >= 2) {
+			int half = arr.length/2;
+			int[] left = Arrays.copyOfRange(arr, 0, half);
+			int[] right = Arrays.copyOfRange(arr, half, arr.length);
+			//System.out.println(half);
+			
+			sort(left);
+			sort(right);
+			merge(arr, left, right);
+		}
 	}
 	
 	private static void merge(int[] arr, int[] left, int[] right) {
-		// TODO Auto-generated method stub
+		int l = 0;
+		int r = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			if ((r >= right.length) || ((l < left.length) && (right[r] > left[l]))) {
+				arr[i] = left[l];
+				l++;
+			}else {
+				arr[i] = right[r];
+				r++;
+			}
+				
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 }
